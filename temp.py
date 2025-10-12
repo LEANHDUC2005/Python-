c6671573128f79db1d619448d91ef3b12a1e125f
LEANHDUC2005 <leanhduc350@gmail.com>
2025-10-13 06:52:03 +0700

    current commit

--- temp.py
+++ temp.py
@@ -1,17 +1,2 @@
-def generate(numRows):
-        """
-        :type numRows: int
-        :rtype: List[List[int]]
-        """
-        Lst = [[1]]
-        if numRows == 1: 
-            return Lst
-        for i in range(numRows - 1):
-            newLst = Lst[i][:]
-            newLst.append(1)
-            for j in range(1, len(Lst[i])):
-                newLst[j] = Lst[i][j-1] + Lst[i][j]
-            Lst.append(newLst)
-        return Lst
-result = generate(3)
-print(result)
+dic = {1: "dic", 2: "student"}
+print(dic[1])
